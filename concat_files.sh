105dd5b697a9cc68cdc1ba9accfe05b6d7bfde18
Georgi Stefanov <go6ostefanov@gmail.com>
2021-01-28 21:18:18 +0200

    Updating video jobs

---
+++ concat_files.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CHUNKS=$1
+LIST_OF_CHUNKS=""
+
+for i in $(seq 1 $(($CHUNKS-1)));
+do
+	LIST_OF_CHUNKS="${LIST_OF_CHUNKS}output${i}|"
+	echo "file output${i}.mp4" >> list-of-chunks.txt
+done
+
+LIST_OF_CHUNKS="${LIST_OF_CHUNKS}output${CHUNKS}"
+
+ffmpeg -f concat -i list-of-chunks.txt -c copy final-output.mp4 || true
+
+rm -f output*.mp4
+
+rm -f list-of-chunks.txt
